e7aac420257c0f0b2d373b252b283384c4419bec
Julio Cesar <julio.druid@gmail.com>
2016-03-10 23:49:36 -0300

    criando rota para produto

--- config/express.js
+++ config/express.js
@@ -1,5 +1,6 @@
 var app = require('express')();
 app.set('view engine', 'ejs');
+app.set('views', './app/views'); // o  caminho é a partir de app.js que sobe o express
 	
 module.exports = function(){
 	return app;	

8dcb2bef7fdf907fb27d0f23397c3cd1c1a8cb50
Julio Cesar <julio.druid@gmail.com>
2016-03-11 23:48:03 -0300

    incluindo mysql

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,5 +1,18 @@
 module.exports = function(app){
 	app.get('/produtos', function(req, res){
+		var mysql = require('mysql');
+		var connection = mysql.createConnection({
+			host: 'localhost',
+			user: 'root',
+			password: '',
+			database: 'casadocodigo_nodejs'	
+		});
+
+		connection.query('select * from livros', function(err, result){
+
+		});
+		connection.end();
 		res.render("produtos/lista");
+
 	});
 }	

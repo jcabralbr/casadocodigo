2f0fb81d3424b303b6ed264c1ce5440300d88f7c
Julio Cesar Cabral <julio.cabral@b2wdigital.com>
2016-04-15 10:26:32 -0300

    config/express.js

--- config/express.js
+++ config/express.js
@@ -1,6 +1,7 @@
-var app = require('express')();
-app.set('view engine', 'ejs');
-app.set('views', './app/views'); // o  caminho é a partir de app.js que sobe o express
+var express = require('express');
 module.exports = function(){
+    var app = express();
+	app.set('view engine', 'ejs');
+	app.set('views', './app/views'); // o  caminho é a partir de app.js que sobe o express
 	return app;
 };
